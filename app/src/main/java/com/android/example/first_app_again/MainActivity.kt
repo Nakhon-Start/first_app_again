71f27ef3ccbd6fa904e8d0ac54bcefcc22021a39
Nakhon-Start <56624645+Nakhon-Start@users.noreply.github.com>
2020-08-24 20:02:13 +0700

    Initial commit

---
+++ app/src/main/java/com/android/example/first_app_again/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.android.example.first_app_again
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
